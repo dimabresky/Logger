ade9873914c6992692e237499d90f0cb63b0bee5
Дмитрий <dimabresky@gmail.com>
2017-11-02 16:36:23 +0300

    Update Logger.php

--- Logger.php
+++ Logger.php
@@ -38,42 +38,6 @@ class Logger {
             if ($fp = @fopen($this->_path2Log, "ab")) {
                 if (flock($fp, \LOCK_EX)) {
                     @fwrite($fp, "Host: ".$_SERVER["HTTP_HOST"]."\nDate: ".date("d.m.Y H:i:s")."\n".$text."\n");
-
-                    $arBacktrace = array_slice(debug_backtrace(true, 7), 1, 7);
-                    $strFunctionStack = "";
-                    $strFilesStack = "";
-                    $firstFrame = (count($arBacktrace) == 1? 0: 1);
-                    $iterationsCount = min(count($arBacktrace), 7);
-                    for ($i = $firstFrame; $i < $iterationsCount; $i++) {
-                        if (strlen($strFunctionStack)>0) {
-                            $strFunctionStack .= " < ";
-                        }
-                        if (isset($arBacktrace[$i]["class"])) {
-                            $strFunctionStack .= $arBacktrace[$i]["class"]."::";
-                        }
-                        $strFunctionStack .= $arBacktrace[$i]["function"];
-
-                        if(isset($arBacktrace[$i]["file"])) {
-                            $strFilesStack .= "\t".$arBacktrace[$i]["file"].":".$arBacktrace[$i]["line"]."\n";
-                        }
-                        if($bShowArgs && isset($arBacktrace[$i]["args"])) {
-                            $strFilesStack .= "\t\t";
-                            if (isset($arBacktrace[$i]["class"])) {
-                                $strFilesStack .= $arBacktrace[$i]["class"]."::";
-                            }
-                            $strFilesStack .= $arBacktrace[$i]["function"];
-                            $strFilesStack .= "(\n";
-                            foreach($arBacktrace[$i]["args"] as $value) {
-                                $strFilesStack .= "\t\t\t".$value."\n";
-                            }
-                            $strFilesStack .= "\t\t)\n";
-                        }
-                    }
-
-                    if (strlen($strFunctionStack)>0) {
-                        @fwrite($fp, "    ".$strFunctionStack."\n".$strFilesStack);
-                    }
-
                     @fwrite($fp, "----------\n");
                     @fflush($fp);
                     @flock($fp, \LOCK_UN);
